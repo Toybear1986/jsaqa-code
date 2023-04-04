6e0997a0b8c6da180af792c687a21a9f3306a232
Василий Скоробогатов <vaskorobogatov@yandex.ru>
2023-04-05 02:26:36 +0300

    7.3.1 Playwright Homework

--- 7.3/playwright/tests/App.spec.js
+++ 7.3/playwright/tests/App.spec.js
@@ -1,22 +1,32 @@
-const { test, expect } = require("@playwright/test");
+const { email } = require("../user");
+const { password } = require("../user");
+import { test, expect } from "@playwright/test";
+import { timeout } from "../playwright.config";
 
-test("test", async ({ page }) => {
-  // Go to https://netology.ru/free/management#/
-  await page.goto("https://netology.ru/free/management#/");
+test("Successfully authorization", async ({ page }) => {
+  await page.goto("https://netology.ru/?modal=sign_in");
+  await page.screenshot({ path: "screenshots/loginpage.png" });
+  await page.getByPlaceholder("Email").click();
+  await page.getByPlaceholder("Email").fill(email);
+  await page.getByPlaceholder("Пароль").click();
+  await page.getByPlaceholder("Пароль").fill(password);
+  await page.getByTestId("login-submit-btn").click();
+  await expect(page).toHaveURL("https://netology.ru/profile");
+  expect(page.getByRole("heading", { name: "Мои курсы и профессии" })).toBeVisible;
+  await page.waitForLoadState('load');
+  await page.screenshot({ path: "screenshots/profile.png", fullPage: true});
+});
 
-  // Click a
-  await page.click("a");
-  await expect(page).toHaveURL("https://netology.ru/");
-
-  // Click text=Учиться бесплатно
-  await page.click("text=Учиться бесплатно");
-  await expect(page).toHaveURL("https://netology.ru/free");
-
-  page.click("text=Бизнес и управление");
-
-  // Click text=Как перенести своё дело в онлайн
-  await page.click("text=Как перенести своё дело в онлайн");
-  await expect(page).toHaveURL(
-    "https://netology.ru/programs/kak-perenesti-svoyo-delo-v-onlajn-bp"
+test("Unsuccess authorization", async ({ page }) => {
+  await page.goto("https://netology.ru/?modal=sign_in");
+  await page.screenshot({ path: "screenshots/loginpage2.png" });
+  await page.getByPlaceholder("Email").click();
+  await page.getByPlaceholder("Email").fill("test@test.com");
+  await page.getByPlaceholder("Пароль").click();
+  await page.getByPlaceholder("Пароль").fill("1q2w3e4r");
+  await page.getByTestId("login-submit-btn").click();
+  await expect(page.getByTestId("login-error-hint")).toHaveText(
+    "Вы ввели неправильно логин или пароль"
   );
+  await page.screenshot({ path: "screenshots/unsuccesslogin.png" });
 });
