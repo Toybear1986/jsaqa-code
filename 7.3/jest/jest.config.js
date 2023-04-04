7de493b0f97cf7d5977b94aa31fa06ab3f0c65c3
Василий Скоробогатов <vaskorobogatov@yandex.ru>
2023-04-04 23:15:34 +0300

    7.3.1 Jest Homework

---
+++ 7.3/jest/jest.config.js
@@ -0,0 +1,18 @@
+/** @type {import('jest').Config} */
+const config = {
+    collectCoverageFrom: [
+      "**/*.{js,jsx}",
+      "!**/node_modules/**",
+      "!**/coverage/**",
+      "!jest.config.js"
+    ],
+    coverageThreshold: {
+      global: {
+        branches: 100,
+        functions: 100,
+        lines: 100,
+      },
+    },
+  };
+  
+  module.exports = config;  
--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -14,4 +14,17 @@ describe("Books names test suit", () => {
       "Гарри Поттер",
     ]);
   });
+  test("Function should throw exception if give array of numbers", () => {
+    const numbers = [10, 5, 0];
+    expect(() => sorting.sortByName(numbers)).toThrow(TypeError);
+  });
+  test("Function should throw exception when use it without args", () => {
+    expect(() => sorting.sortByName()).toThrow(TypeError);
+  });
+  test("Function should not sort equals elements", () => {
+    const input = ["Гарри Поттер", "Властелин Колец", "Властелин Колец"];
+    const expected = ["Властелин Колец", "Властелин Колец", "Гарри Поттер"];
+    const result = sorting.sortByName(input);
+    expect(result).toEqual(expected);
+  });
 });
